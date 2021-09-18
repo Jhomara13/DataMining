4fa7668abde0b5b4add32f1ac5d10a63d39a709d
gregorio1297 <gregorio.guzman17@tectijuana.edu.mx>
2021-09-17 19:56:03 -0700

    Create the file Practice2 and add 10 functions

---
+++ Practices/Practice2.R
@@ -0,0 +1,97 @@
+Functions
+
+Practice find 20 more functions in R and make an example of it.
+
+#1 Rnorm:this function takes n random number from -1 to 1 
+
+#Example: 
+rnorm(2)
+
+#2 Sqrt: this function allows you to calculate the square root of a number.
+
+#Example:
+sqrt(9)
+
+#3 Paste: this function concatenate vectors after converting to character.
+
+#Example:
+message<-paste('Hello','World')
+
+
+#4 C: this function allows you to combine and create a vector
+
+#Example:
+
+vec <- c(3,45,77)
+
+#5 Seq: this function allows you to generate a sequence
+
+#Example:
+seq(1,5)
+
+#6 Rep: this function allows to repeat a number n number of times, first we place the number then the quantity.
+
+#Example:
+rep(2,10)
+
+#7 Sum: this function sums all the values within the range you set.
+
+#Example:
+sum(25:80)
+
+
+#8 length:this function shows the number of elements in a vector.
+
+#Example:
+x<-c(1,2,3,4)
+length(x)
+
+#9 trunc: this function removes decimals from a number.
+
+#Example:
+trunc(2.987)
+
+#10 round: this function rounds a number to the specified decimal places.
+
+#Example:
+round(7.952,digits=2)
+
+#11
+
+#Example:
+
+#12
+
+#Example:
+
+#13
+
+#Example:
+
+#14
+
+#Example:
+
+#15
+
+#Example:
+
+#16
+
+#Example:
+
+#17
+
+#Example:
+
+#18
+
+#Example:
+
+#19
+
+#Example:
+
+#20
+
+#Example:
